25c8e15504e59b8a1a48e9a77a18eafebe2c1361
legendary <145838047+legendryPro@users.noreply.github.com>
2024-05-18 16:29:28 +0530

    Update gemini.js

--- src/lib/gemini.js
+++ src/lib/gemini.js
@@ -8,7 +8,7 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = "YOUR_API";
+const API_KEY = "AIzaSyDcPRUG8oVlZwZKJVxGwXRmuwV9umHZVvI";
 
 async function runChat(prompt) {
   const genAI = new GoogleGenerativeAI(API_KEY);
